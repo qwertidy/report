8924cf141eea6021ab4787d4e1738552b317e3d8
qwertidy <113405420+qwertidy@users.noreply.github.com>
2024-04-01 17:08:01 +0800

    Add files via upload

---
+++ import math.py
@@ -0,0 +1,10 @@
+import math
+mark = {'A':84,'B':90,'C':75,'D':82}
+print("最高分為：")
+print(max(mark.values()))
+print("最低分為：")
+print(min(mark.values()))
+print("人數為：")
+print(len(mark))
+print("平均分：")
+print(sum(mark.values())/len(mark))
